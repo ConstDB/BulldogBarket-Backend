4b51e7539e670abd59f26b9b22269d6b24ab31ad
Jonel Villaver <jonelvillaver735@gmail.com>
2025-12-03 13:17:00 +0800

    refactor(auth/signup): migrate hashing and uniqueness checks to service layer

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,58 +6,53 @@ import { UserDoc } from "../types/userDoc";
 import { getRandomAdventurerAvatar } from "../utils/dicebear";
 import { hashPassword } from "../utils/hash";
 
-const userSchema = new Schema<UserDoc>({
-  name: {
-    type: String,
-    required: [true, "A user must have a name."],
+const userSchema = new Schema<UserDoc>(
+  {
+    name: {
+      type: String,
+      required: [true, "A user must have a name."],
+    },
+    studentNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      immutable: true,
+    },
+    course: {
+      type: String,
+      enum: NU_COURSES,
+      required: true,
+    },
+    yearLevel: {
+      type: String,
+      enum: NU_YEAR_LEVELS,
+      required: [true, "A user must have year level."],
+    },
+    campus: {
+      type: String,
+      enum: NU_CAMPUSES,
+      required: [true, "A user must have campus."],
+      default: "NU-Manila",
+    },
+    password: {
+      type: String,
+      required: [true, "A user must have password."],
+      minlength: 8,
+      select: false,
+    },
+    avatarUrl: {
+      type: String,
+      default: getRandomAdventurerAvatar(),
+    },
+    socials: {
+      messengerLink: { type: String, default: "" },
+    },
+    itemsSold: {
+      type: Number,
+      default: 0,
+    },
   },
-  studentNumber: {
-    type: String,
-    required: true,
-    unique: true,
-    immutable: true,
-  },
-  course: {
-    type: String,
-    enum: NU_COURSES,
-    required: true,
-  },
-  yearLevel: {
-    type: String,
-    enum: NU_YEAR_LEVELS,
-    required: [true, "A user must have year level."],
-  },
-  campus: {
-    type: String,
-    enum: NU_CAMPUSES,
-    required: [true, "A user must have campus."],
-    default: "NU-Manila",
-  },
-  password: {
-    type: String,
-    required: [true, "A user must have password."],
-    minlength: 8,
-    select: false,
-  },
-  avatarUrl: {
-    type: String,
-    default: getRandomAdventurerAvatar(),
-  },
-  socials: {
-    messengerLink: { type: String, default: "" },
-  },
-  itemsSold: {
-    type: Number,
-    default: 0,
-  },
-});
-
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
-
-  if (this.password) {
-    this.password = await hashPassword(this.password);
-  }
-});
+  { timestamps: true }
+);
 
 export const UserModel = model<UserDoc>("User", userSchema);
