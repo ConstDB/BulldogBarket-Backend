bdc669253625e225a28ccfbf463efb42c2b847e4
Jonel Villaver <jonelvillaver735@gmail.com>
2025-12-06 02:28:48 +0800

    feat(schema/listing): redesign Product to interactive Listing model

---
+++ src/models/listing.model.ts
@@ -0,0 +1,65 @@
+import { model, Schema, Types } from "mongoose";
+
+export const CommentSchema = new Schema(
+  {
+    user: { type: Types.ObjectId, required: true, ref: "User" },
+    message: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { _id: false }
+);
+
+export const ListingSchema = new Schema(
+  {
+    seller: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["single", "bulk"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "A product must have a name."],
+    },
+    images: {
+      type: [String],
+      required: [true, "A product must have an image"],
+    },
+    price: {
+      type: Number,
+      required: [true, "A product must have a price"],
+    },
+    category: {
+      type: String,
+      required: [true, "A product must have a category"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // ADDITIONAL FIELD FOR BULK
+    stocks: {
+      type: Number,
+      required: false,
+    },
+    // ADDITIONAL FIELD FOR SINGLE
+    condition: {
+      type: String,
+      required: false,
+    },
+    upvotes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [CommentSchema],
+  },
+  { timestamps: true }
+);
+
+export const ListingModel = model("Listing", ListingSchema);
