b6b1beb92ba762730d0a07529f88bd4b2f1aed46
Jonel Villaver <jonelvillaver735@gmail.com>
2025-12-08 06:45:46 +0800

    feat(order): implement order creation route and validation schema

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,9 @@ import { env } from "./config/env";
 import { globalErrorHandler } from "./middlewares/error.middleware";
 import { sanitize } from "./middlewares/sanitize";
 import authRoutes from "./routes/auth.routes";
-import userRoutes from "./routes/user.routes";
 import listingRoutes from "./routes/listing.routes";
+import orderRoutes from "./routes/order.routes";
+import userRoutes from "./routes/user.routes";
 import { AppError } from "./utils/appError";
 
 const app = express();
@@ -42,7 +43,8 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/listings", listingRoutes);
-app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/orders", orderRoutes);
 
 app.use("/{*any}", (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
