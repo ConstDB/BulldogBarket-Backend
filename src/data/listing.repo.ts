92ed5bef161ba7284561c5fb5db2a3d88baa3a00
Jonel Villaver <jonelvillaver735@gmail.com>
2025-12-07 19:43:45 +0800

    feat(repository/listing): implement paginated feed retrieval method

--- src/data/listing.repo.ts
+++ src/data/listing.repo.ts
@@ -1,11 +1,28 @@
 import { Types } from "mongoose";
 import { ListingModel } from "../models/listing.model";
 import { ListingDoc } from "../types/listingDoc";
-import { CreateListing } from "../validations/listing";
+import { CreateListing, ListingQuery } from "../validations/listing";
 
 export const ListingRepository = {
   create: async (data: CreateListing, sellerId: Types.ObjectId): Promise<ListingDoc> => {
     const listing = await ListingModel.create({ ...data, seller: sellerId, upvotes: [], comments: [] });
     return listing;
+  },
+
+  getFeed: async (options: ListingQuery) => {
+    const { page, limit, sort } = options;
+    const query: any = {};
+
+    let sortOptions: any = { createdAt: -1 };
+
+    if (sort === "popular") sortOptions = { upvotes: -1, createdAt: -1 };
+
+    const listings = await ListingModel.find(query)
+      .sort(sortOptions)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .populate("seller", "name avatarUrl yearLevel course campus socials.messengerLink");
+
+    return listings;
   },
 };
