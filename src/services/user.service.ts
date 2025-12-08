13e3c8daa5203923a2df8348428c54ca68c9f1d7
AlammobaDar <162420273+alammobaDar@users.noreply.github.com>
2025-12-08 22:13:53 +0800

    refactor response body implementation for user profile feature

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,13 +2,20 @@ import { Types } from "mongoose"
 import { UserRepository } from "../data/user.repo"
 import { updateUserProfile } from "../validations/user";
 import { toUserProfileResponse } from "../dto/user.dto";
+import { NotFoundError } from "../utils/appError";
 
 export const getUserProfileService = async (id: Types.ObjectId) =>{
     const user = await UserRepository.findById(id);
-    return toUserProfileResponse(user!);
+    if (!user){
+        throw new NotFoundError();
+    }
+    return toUserProfileResponse(user);
 }
 
 export const updateUserProfileService = async (id: Types.ObjectId, data: updateUserProfile) => {
     const user = await UserRepository.update(id, data);
-    return toUserProfileResponse(user!);
+    if (!user){
+        throw new NotFoundError();
+    }
+    return toUserProfileResponse(user);
 }
