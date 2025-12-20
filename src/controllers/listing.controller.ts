7877a190dc25f1c1e3f421f4ebebc5aa9af55c2a
Jonel Villaver <jonelvillaver735@gmail.com>
2025-12-21 04:30:02 +0800

    refactor(service/listing): group listing services into ListingService object

--- src/controllers/listing.controller.ts
+++ src/controllers/listing.controller.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
 import { toListingFeedResponse } from "../dto/listing.dto";
-import { createListingService, downvotesServices, getListingsFeedService, upvotesService } from "../services/listing.service";
+import { ListingService } from "../services/listing.service";
 import { asyncHandler } from "../utils/asyncHandlers";
 import { Types } from "mongoose";
 
 export const createListing = asyncHandler(async (req: Request, res: Response) => {
-  const listing = await createListingService(req.body, req.user._id);
+  const listing = await ListingService.createListing(req.body, req.user._id);
   res.status(201).json({ ...listing });
 });
 
 export const getListingFeed = asyncHandler(async (req: Request, res: Response) => {
   const query = req.validatedQuery;
 
-  const feed = await getListingsFeedService({
+  const feed = await ListingService.getListingsFeed({
     page: query.page,
     limit: query.limit,
     sort: query.sort,
@@ -27,18 +27,19 @@ export const upvotes = asyncHandler(async (req: Request, res: Response) => {
   if (!listing) {
     return res.status(400).json({ message: "Failed to upvote." });
   }
-  
-  await upvotesService(req.user._id, listing);
+
+  await ListingService.upvote(req.user._id, listing);
   return res.sendStatus(204);
-})
+});
 
 export const downvotes = asyncHandler(async (req: Request, res: Response) => {
   const listing = new Types.ObjectId(req.params.id);
 
-  if (!listing){
-    return res.status(400).json({ message: "Failed to downvote."});
+  if (!listing) {
+    return res.status(400).json({ message: "Failed to downvote." });
   }
 
-  await downvotesServices(req.user._id, listing);
-  return res.sendStatus(204)
-})
+  await ListingService.downvote(req.user._id, listing);
+  return res.sendStatus(204);
+});
+
--- src/services/listing.service.ts
+++ src/services/listing.service.ts
@@ -3,21 +3,24 @@ import { ListingRepository } from "../data/listing.repo";
 import { toListingRepsonse } from "../dto/listing.dto";
 import { CreateListing, ListingQuery } from "../validations/listing";
 
-export const createListingService = async (data: CreateListing, id: Types.ObjectId) => {
-  const listing = await ListingRepository.create(data, id);
-  return toListingRepsonse(listing);
+export const ListingService = {
+  // TODO: Refactor this create listing, add checks to ensure that the user is creating a valid listing
+  createListing: async (data: CreateListing, id: Types.ObjectId) => {
+    const listing = await ListingRepository.create(data, id);
+    return toListingRepsonse(listing);
+  },
+
+  getListingsFeed: async (data: ListingQuery) => {
+    return ListingRepository.getFeed(data);
+  },
+
+  upvote: async (userId: Types.ObjectId, listingId: Types.ObjectId) => {
+    const upvotes = await ListingRepository.upvote(userId, listingId);
+    return upvotes;
+  },
+
+  downvote: async (userId: Types.ObjectId, listingId: Types.ObjectId) => {
+    const downvotes = await ListingRepository.downvote(userId, listingId);
+    return downvotes;
+  },
 };
-
-export const getListingsFeedService = async (data: ListingQuery) => {
-  return ListingRepository.getFeed(data);
-};
-
-export const upvotesService = async (userId: Types.ObjectId, listingId: Types.ObjectId) => {
-  const upvotes = await ListingRepository.upvote(userId, listingId);
-  return upvotes
-}
-
-export const downvotesServices = async (userId: Types.ObjectId, listingId: Types.ObjectId) => {
-  const downvotes = await ListingRepository.downvote(userId, listingId);
-  return downvotes
-}
